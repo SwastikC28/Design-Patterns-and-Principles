f54bacd39c182f4b083ba99d9b66dbab69119ea0
Swastik Chaudhary <swastikkumar50@gmail.com>
2025-02-03 02:21:12 +0530

    MachineCoding : VendingMachine - Converted into cmd user based program

--- MachineCoding/VendingMachine/cmd/main.go
+++ MachineCoding/VendingMachine/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"vendingmachine/internal/model"
 )
 
@@ -28,16 +29,67 @@ func main() {
 	vendingMachineBank := model.NewVendingMachineBank()
 	vendingMachine := model.NewVendingMachine(inventory, vendingMachineBank)
 
-	vendingMachine.DisplayProducts()
-	err := vendingMachine.BuyProduct("Lays", 1, user.GetPaymentMode(model.CREDIT_CARD))
-	if err != nil {
-		fmt.Println(err)
-	}
+	for {
+		fmt.Println("\nAvailable actions:")
+		fmt.Println("1. Display Products")
+		fmt.Println("2. Buy Product")
+		fmt.Println("3. Add Credit to Card")
+		fmt.Println("4. Show Sales")
+		fmt.Println("5. Exit")
 
-	user.GetPaymentMode(model.CREDIT_CARD).AddBalance(100)
-	err = vendingMachine.BuyProduct("Lays", 1, user.GetPaymentMode(model.CREDIT_CARD))
-	if err != nil {
-		fmt.Println(err)
-	}
+		fmt.Print("\nChoose an option: ")
+		var choice int
+		fmt.Scan(&choice)
 
+		switch choice {
+		case 1:
+			vendingMachine.DisplayProducts()
+
+		case 2:
+			fmt.Print("Enter the product name: ")
+			var productName string
+			fmt.Scan(&productName)
+
+			fmt.Print("Enter the quantity: ")
+			var quantity int
+			fmt.Scan(&quantity)
+
+			fmt.Println("Choose payment method:")
+			fmt.Println("1. Credit Card")
+			fmt.Println("2. Cash")
+			fmt.Print("Enter payment method (1/2): ")
+			var paymentChoice int
+			fmt.Scan(&paymentChoice)
+
+			var paymentMethod model.PaymentMode
+			if paymentChoice == 1 {
+				paymentMethod = user.GetPaymentMode(model.CREDIT_CARD)
+			} else {
+				paymentMethod = user.GetPaymentMode(model.CASH)
+			}
+
+			err := vendingMachine.BuyProduct(productName, quantity, paymentMethod)
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+
+		case 3:
+			fmt.Print("Enter amount to add to credit card: ")
+			var amount float64
+			fmt.Scan(&amount)
+
+			user.GetPaymentMode(model.CREDIT_CARD).AddBalance(int(amount))
+			fmt.Println("Amount added to credit card.")
+
+		case 4:
+			vendingMachine.ShowVendingMachineSales()
+
+		case 5:
+			fmt.Println("Exiting...")
+			os.Exit(0)
+
+		default:
+			fmt.Println("Invalid option. Please try again.")
+		}
+	}
 }
